96852e5140a85aae25c174946af4c1fa1617ed7b
Leonardo Inácio <winchester447@hotmail.com>
2020-09-01 21:42:46 -0300

    css/estilo.css

--- js/script.js
+++ js/script.js
@@ -1,6 +1,6 @@
 
 var botoes = document.getElementsByTagName('button');
 botoes[0].addEventListener('click', function() {
-    window.alert('seja bem vindo otario');
+    window.alert('seja bem vindo');
 });
  
